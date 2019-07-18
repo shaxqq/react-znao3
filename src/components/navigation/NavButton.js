d0d263b2063b1da84b6c98415c675b7885fd116e
shaxqq <39559342+shaxqq@users.noreply.github.com>
2019-07-18 20:05:56 +0300

    navigation/reglament/loginform

--- src/components/navigation/NavButton.js
+++ src/components/navigation/NavButton.js
@@ -1,49 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, forwardRef } from "react";
 import IconButton from "@material-ui/core/IconButton";
-import Menu from "@material-ui/core/Menu";
+import Slide from "@material-ui/core/Slide";
 import AccountCircle from "@material-ui/icons/AccountCircle";
-import MenuItem from "@material-ui/core/MenuItem";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function NavButton() {
-  const [anchorEl, setAnchorEl] = useState(null);
+const useStyle = makeStyles(theme => ({
+  userButton: {
+    color: "#2196f3"
+  },
+  icon__button: {
+    marginLeft: theme.spacing(2)
+  }
+}));
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+const NavButton = () => {
+  const classes = useStyle;
   const [open, setOpen] = useState(false);
 
-  const logInOpen = () => {
+  function handleClickOpen() {
     setOpen(true);
-  };
-
-  const logInClose = () => {
-    setOpen(false);
-  };
-
-  function handleClick(e) {
-    setAnchorEl(e.currentTarget);
   }
   function handleClose() {
-    setAnchorEl(null);
+    setOpen(false);
   }
 
   return (
     <div>
       <IconButton
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
+        className={classes.icon__button}
+        style={{ marginLeft: "18px" }}
         color="inherit"
         edge="end"
-        style={{ marginLeft: "16px" }}
+        onClick={handleClickOpen}
       >
         <AccountCircle />
       </IconButton>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
+
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
         onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
       >
-        <MenuItem onClick={handleClose}>Вход</MenuItem>
-        <MenuItem onClick={handleClose}>Регистрация</MenuItem>
-      </Menu>
+        <DialogActions style={{ justifyContent: "space-around" }}>
+          <Button variant="contained" className={classes.userButton}>
+            Войти
+          </Button>
+          <Button color="primary">Регистрация</Button>
+        </DialogActions>
+
+        <DialogTitle>dfsg</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+      </Dialog>
     </div>
   );
-}
+};
+export default NavButton;
